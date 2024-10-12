554772f62d8606856c9bea667600803f75b508d8
uddesh7 <7uddesh@gmail.com>
2024-10-13 01:25:21 +0530

    episode 1 final code

--- App.js
+++ App.js
@@ -1,3 +1,28 @@
-const heading = React.createElement("h1", {}, "HEllo world from react");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+{
+  /* <div id='parent'>
+    <div id='child'>
+        <h1></h1>
+        <h2></h2>
+    </div>
+    <div id='child'>
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div> */
+}
+
+//creating the html structure as above
+const element = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is an h1 tag"),
+    React.createElement("h2", {}, "this is a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is an h1 tag"),
+    React.createElement("h2", {}, "this is a h2 tag"),
+  ]),
+]);
+
+// const heading = React.createElement("h1", {id: 'heading', prop: 'prop'}, "Hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
